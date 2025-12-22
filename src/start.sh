0b35a171096c24780f044257adc277a434586a3f
neutrino2211 <neutrino2211@gmail.com>
2025-12-22 23:16:38 +0100

    Update start.sh

--- src/start.sh
+++ src/start.sh
@@ -12,12 +12,16 @@ trap cleanup SIGINT SIGTERM
 # Kill any existing inflect processes
 pgrep inflect | xargs kill
 
+ls -l /runpod-volume
+ls -l /runpod-volume/$INFLECT_SYSTEM_NAME
+
 # Start the inflect server and log its output
 inflect serve -d /runpod-volume/$INFLECT_SYSTEM_NAME 2>&1 | tee inflect.server.log &
 INFLECT_PID=$! # Store the process ID (PID) of the background command
 
 check_server_is_running() {
     echo "Checking if server is running..."
+    cat inflect.server.log
     if cat inflect.server.log | grep -q "Server running at"; then
         return 0 # Success
     else
